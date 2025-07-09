f1dfb59bd2981a13c9f1799822ae2378df94147d
alex052525 <alex62626@naver.com>
2025-07-09 13:24:27 +0900

    [FEAT] 유저 중복 응모 방지 로직 구현

--- src/main/java/com/team3/fastpick/service/DrawService.java
+++ src/main/java/com/team3/fastpick/service/DrawService.java
@@ -9,7 +9,8 @@ import com.team3.fastpick.dto.response.DrawResponse;
 public class DrawService {
     private static final String ATTEMPT_PREFIX = "draw:";             // 응모 기록
     private static final String COUNT_PREFIX = "draw_count:";         // 응모 카운트
-    private static final int MAX_COUNT = 10;
+    private static final String USER_SET_PREFIX = "draw_users:";       // 유저 중복 방지용
+    private static final int MAX_COUNT = 3;
 
     private final RedisTemplate<String, Object> redisTemplate;
 
@@ -18,20 +19,35 @@ public class DrawService {
     }
 
     public DrawResponse requestDraw(int productId, int userId) {
+        
+        String userSetKey = buildUserSetKey(productId);
+        Boolean alreadyParticipated = redisTemplate.opsForSet().isMember(userSetKey, userId);
+
+        if (Boolean.TRUE.equals(alreadyParticipated)) {
+            return new DrawResponse(false, "이미 응모된 사용자입니다.");
+        }
+        
         String countKey = buildCountKey(productId);
         Long currentCount = incrementCount(countKey);
 
         if (currentCount > MAX_COUNT) {
             decrementCount(countKey);
-            return new DrawResponse(false, "이미 해당 상품에 대한 응모 횟수 한도를 초과했습니다.");
+            return new DrawResponse(false, "마감됐습니다.");
         }
 
         saveUserAttempt(productId, currentCount, userId);
-        return new DrawResponse(true, "응모 성공. 현재 전체 응모 수: " + currentCount);
+        
+        // 4. 중복 방지를 위해 유저 ID 저장
+        recordUserParticipation(userSetKey, userId); // TTL 설정은 필요시 추가
+        return new DrawResponse(true, "당첨됐습니다.");
     }
 
     private String buildCountKey(int productId) {
         return COUNT_PREFIX + productId;
+    }
+    
+    private String buildUserSetKey(int productId) {
+        return USER_SET_PREFIX + productId;
     }
 
     private Long incrementCount(String countKey) {
@@ -45,6 +61,10 @@ public class DrawService {
     private void saveUserAttempt(int productId, Long attemptNumber, int userId) {
         String attemptKey = ATTEMPT_PREFIX + productId + ":" + attemptNumber;
         redisTemplate.opsForValue().set(attemptKey, String.valueOf(userId));
+    }
+    
+    private void recordUserParticipation(String userSetKey, int userId) {
+        redisTemplate.opsForSet().add(userSetKey, userId);
     }
 }
 
