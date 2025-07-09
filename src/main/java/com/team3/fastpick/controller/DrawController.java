3bc3c45a7f5a01ecb5fd7308a4953a4e32707809
alex052525 <alex62626@naver.com>
2025-07-09 16:36:39 +0900

    [FIX] 테이블 ID 컬럼명 네이밍 변경

--- src/main/java/com/team3/fastpick/controller/DrawController.java
+++ src/main/java/com/team3/fastpick/controller/DrawController.java
@@ -17,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DrawController {
 	private final DrawService drawService;
+	private static final Long pidx = 1L; // 예시로 고정된 상품 ID 사용
 	
 	@PostMapping("/draw")
     public String draw(@ModelAttribute DrawRequest req, Model model) {
@@ -27,9 +28,8 @@ public class DrawController {
 	
 	@GetMapping("/draw-page")
 	public String drawPageForm(Model model) {
-		Long pidx = 1L; // 예시로 고정된 상품 ID 사용
 		DrawInfo drawInfo = drawService.getDrawInfo(pidx);
 		model.addAttribute("drawInfo", drawInfo);
-		return "draw-page";
+		return "drawInfo";
 	}
 }
