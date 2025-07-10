b811c9d29e79dcd4f5bd423e59059d0436979d09
alex052525 <alex62626@naver.com>
2025-07-10 09:30:44 +0900

    [FIX] pidx를 고정값에서 동적으로 변경

--- src/main/java/com/team3/fastpick/controller/DrawController.java
+++ src/main/java/com/team3/fastpick/controller/DrawController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.team3.fastpick.dto.DrawInfo;
@@ -19,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DrawController {
 	private final DrawService drawService;
-	private static final Long pidx = 1L; // 예시로 고정된 상품 ID 사용
+//	private static final Long pidx = 1L; // 예시로 고정된 상품 ID 사용
 	
 	@PostMapping("/draw")
     public String draw(@ModelAttribute DrawRequest req, Model model) {
@@ -29,7 +30,8 @@ public class DrawController {
     }
 	
 	@GetMapping("/draw-page")
-	public String drawPageForm(HttpSession session, Model model) {
+	public String drawPageForm(HttpSession session, Model model, @PathVariable("pidx") Long pidx) {
+		
 		
 		// 로그인한 사용자 정보 꺼내기
 	    User user = (User) session.getAttribute("loginUser");
